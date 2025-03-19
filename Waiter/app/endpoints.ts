2aa7a380e76f80f802d3bbc8b2b0acd4732085dc
Dere-Wah <derexcontact@gmail.com>
2025-03-19 23:34:57 +0100

    update port to 8585

--- Waiter/app/endpoints.ts
+++ Waiter/app/endpoints.ts
@@ -1,3 +1,3 @@
-export const BACKEND_SERVER = "https://waiter.derewah.dev:8888";
-export const WS_SERVER = "wss://waiter.derewah.dev:8888";
+export const BACKEND_SERVER = "https://waiter.derewah.dev:8585";
+export const WS_SERVER = "wss://waiter.derewah.dev:8585";
 export const APP_URL = "https://waiter.derewah.dev:3000";
