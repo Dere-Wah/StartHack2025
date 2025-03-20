cbada513fea0c1fbd79a2722eacac12fe29f16ac
Dere-Wah <derexcontact@gmail.com>
2025-03-20 14:39:29 +0100

    update endpoints

--- Waiter/app/endpoints.ts
+++ Waiter/app/endpoints.ts
@@ -1,5 +1,5 @@
 export const BACKEND_SERVER = "https://waiter-api.derewah.dev";
 export const WS_SERVER = "wss://waiter-api.derewah.dev";
-//export const APP_URL = "https://waiter.derewah.dev";
+export const APP_URL = "https://waiter.derewah.dev";
 
-export const APP_URL = "https://waiter-api.derewah.dev";
+//export const APP_URL = "http://localhost:3000";
